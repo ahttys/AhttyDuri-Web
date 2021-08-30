8931585100655f6a3014563c0eea0d428f0d0e67
woojerry <woojerry@naver.com>
2021-08-30 23:24:46 +0900

    Navbar media query added

--- src/components/molecules/Navbar/style.ts
+++ src/components/molecules/Navbar/style.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Button from "../../atoms/Button";
+//import { Button } from "components";
 
 export const NavContainer = styled.div`
   display: flex;
@@ -7,12 +8,20 @@ export const NavContainer = styled.div`
   align-items: center;
   height: 60px;
   box-shadow: rgb(0 0 0 / 8%) 0px 1px 0px 0px;
+
+  @media only screen and (max-width: 768px) {
+    height: auto;
+  }
 `;
 
 export const StyledNav = styled.nav`
   width: 1280px;
   display: flex;
   justify-content: space-between;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    flex-direction: column;
+  }
 `;
 
 export const LogoContainer = styled.div``;
@@ -26,6 +35,9 @@ export const StyledUl = styled.ul`
   margin: 0;
   padding: 0;
   list-style: none;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 export const StyledLi = styled.li`
@@ -33,6 +45,8 @@ export const StyledLi = styled.li`
   margin-left: 10px;
   display: flex;
   align-items: center;
+  flex-direction: column;
+  text-align: center;
 `;
 
 export const StyledA = styled.a`
@@ -41,10 +55,16 @@ export const StyledA = styled.a`
   &:hover {
     color: ${(props) => props.theme.palette.gray};
   }
+
+  @media only screen and (max-width: 768px) {
+    display: block;
+    padding: 8px;
+  }
 `;
 
-export const ButtonContainer = styled.div`
-  display: flex;
+export const StyledButton = styled(Button)`
+  @media only screen and (max-width: 768px) {
+    width: 30%;
+    margin: 5px auto;
+  }
 `;
-
-export const StyledButton = styled(Button)``;
