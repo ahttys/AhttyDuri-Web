00b496e4d358c9d31cc86b2fe289ecffb123fd77
woojerry <woojerry@naver.com>
2021-09-06 11:27:33 +0900

    gitignore test4

--- src/common/constants/kako.ts
+++
@@ -1,4 +0,0 @@
-const CLIENT_ID = "d422945395b059d9c25bcbf1674cbf3f";
-const REDIRECT_URI = "http://localhost:3000/";
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
